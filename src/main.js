12b6353024b344063a0ac7d8ac7885dd65ec24ab
julius bade <julbade21@gmail.com>
2018-10-16 16:21:54 -0700

    add css

--- src/main.js
+++ src/main.js
@@ -4,24 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-    $('#weatherLocation').click(function() {
-      let city = $('#location').val();
-      $('#location').val("");
-      $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e133ad6429b649086da1c6a3503ed04d`,
-        type: 'GET',
-        data: {
-          format: 'json'
-        },
-        success: function(response) {
-          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-          $('.showTemp').text(`The temperature in Celsius is ${response.main.temp}.`);
-        },
-        error: function() {
-          $('#errors').text("There was an error processing your request. Please try again.");
-        }
+  $('#weatherLocation').click(function(event) {
+    event.preventDefault()
+    const city = $('#location').val();
+    $('#location').val("");
 
-        
-      });
-    });
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e133ad6429b649086da1c6a3503ed04d`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Celsius is ${response.main.temp} degrees.`);
+    }
   });
+});
--- src/tracker.js
+++ src/tracker.js
@@ -1,17 +1,3 @@
-// var jupiter;
-
-// var api = 'https://api.nasa.gov/planetary/apod?api_key=' ;
-// var apiKey = "q8r0Xtu0yBep7SZVA8tEhyy0FX77hWUpzKDkOSGt";
-
-
-// function setup() {
-
-// }
-
-// function gotData (data) {
-//     jupiter = data;
-// }
-
 
 let response;
 
@@ -28,5 +14,5 @@ getElements(response);
 
 const getElements = function (response) {
     $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    $('.showTemp').text(`The temperature in Celsius ${response.main.temp} degrees.`);
 }
